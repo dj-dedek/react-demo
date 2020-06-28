c95e303e95c72dbee0cb679fb20d2377e68896d8
dj-dedek <dedekitsjay@gmail.com>
2020-06-28 21:57:58 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import HighScore from './HighScore';
+import './css/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            count: 0,
+            overTen: false
+        }
+    }
+
+    handleButton = () => {
+        this.setState({
+            count : this.state.count + 1
+        })
+    }
+
+    componentDidUpdate() {
+        if(this.state.count > 10 && this.state.count && !this.state.overTen) {
+            console.log("Updating over ten");
+            this.setState({
+                overTen: true
+            });
+        }
+    }
+
+    onReset = () => {
+        this.setState({
+            count: 0,
+            overTen: false
+        })
+    }
+
+    render() {
+        let {count} = this.state;
+
+        return(
+            <div>
+                <h1 class="test-ee">You clicked the button {count} times</h1>
+                <HighScore 
+                    overTen={this.state.overTen}
+                    onReset={(e) => this.onReset(e)}
+                />
+                <span>
+                    <button onClick={() => this.handleButton()}>Click Me!</button>
+                </span>
+            </div>
+        );
+    }
 }
 
 export default App;
